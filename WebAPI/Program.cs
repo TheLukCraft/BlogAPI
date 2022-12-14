c8cc542d28947f56d487ae552e0ef7b367c4aaa0
TheLukCraft <luk1997r@o2.pl>
2022-12-14 01:20:54 +0100

    Downloading resource using GET method with automapper

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,11 @@
+using Application.Interfaces;
+using Application.Mappings;
+using Application.Services;
+using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Repositories;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +13,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.EnableAnnotations();
+    x.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "V1" });
+});
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<IPostService, PostService>();
+builder.Services.AddSingleton(AutoMapperConfig.Initialize());
 
 var app = builder.Build();
 
